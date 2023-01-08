1b4a6041010992fd96df616f94d20c273e0f1ffb
ZhjMr <2744764939@qq.com>
2023-01-08 13:45:54 +0800

    刷新全屏功能

--- src/layout/components/AppHeader.vue
+++ src/layout/components/AppHeader.vue
@@ -6,16 +6,24 @@
       积云商城
     </span>
     <!-- 菜单展开或收起的图标 -->
-    <el-icon class="icon-btn"><Fold /></el-icon>
+    <el-tooltip effect="dark" content="菜单" placement="bottom">
+      <el-icon class="icon-btn"><Fold /></el-icon>
+    </el-tooltip>
     <!-- 全局刷新图标 -->
-    <el-icon class="icon-btn"><Refresh /></el-icon>
+    <el-tooltip effect="dark" content="刷新" placement="bottom">
+      <el-icon class="icon-btn" @click="handleRefsh"><Refresh /></el-icon>
+    </el-tooltip>
 
     <div class="ml-auto dropdown">
       <!-- 全屏图标 -->
-      <el-icon class="icon-btn"><FullScreen /></el-icon>
+      <el-tooltip effect="dark" content="全屏" placement="bottom">
+        <el-icon class="icon-btn" @clikc="toggle">
+          <FullScreen v-if="!isFullscreen" />
+          <Aim v-else />
+        </el-icon>
+      </el-tooltip>
       <!-- 下拉菜单 -->
-
-      <el-dropdown class="dro">
+      <el-dropdown class="dro" @command="handleCommand">
         <span class="el-dropdown-link">
           <el-avatar class="mr-2" :size="25" :src="$store.state.user.avatar" />
           {{ $store.state.user.username }}
@@ -25,8 +33,8 @@
         </span>
         <template #dropdown>
           <el-dropdown-menu>
-            <el-dropdown-item>修改密码</el-dropdown-item>
-            <el-dropdown-item>退出登录</el-dropdown-item>
+            <el-dropdown-item command="rePassword">修改密码</el-dropdown-item>
+            <el-dropdown-item command="logout"> 退出登录</el-dropdown-item>
           </el-dropdown-menu>
         </template>
       </el-dropdown>
@@ -37,19 +45,23 @@
 import { useStore } from "vuex";
 import { taost, showModel } from "@/composables/utils.js";
 import { useRouter } from "vue-router";
+import { useFullscreen } from "@vueuse/core";
+// isFullscreen 是否是全屏 enter 进入全屏 exit退出全屏 toggle全屏切换
+const { isFullscreen, enter, exit, toggle } = useFullscreen();
 //初始化store
 const store = useStore();
 //初始化路由
 const router = useRouter();
 // 修改密码  退出登录
-const handleCommand = (e) => {
-  console.log(e);
-  // 退出登录
-  if (e === "logout") {
-    handleLogout();
-  } else {
-    //修改密码
-    handleChangePass();
+const handleCommand = (command) => {
+  switch (command) {
+    case "logout":
+      // logout调用退出登录方法
+      handleLogout();
+      break;
+    case "rePassword":
+      // rePassword调用修改密码方法
+      handleChangePass();
   }
 };
 //点击退出登录触发的方法
@@ -68,8 +80,12 @@ const handleLogout = () => {
 const handleChangePass = () => {
   alert("修改密码");
 };
+//点击刷新触发的方法
+const handleRefsh = () => {
+  location.reload();
+};
 </script>
-<style scoped>
+<style lang="postcss" scoped>
 .Appheader {
   height: 64px;
   @apply flex bg-indigo-700 items-center fixed right-0 left-0 top-0 text-light-50;
